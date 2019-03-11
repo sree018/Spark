a21dac3262e500c75e5a43bcf4345c21484b304f
sree018 <sdama018@gmail.com>
2019-03-11 12:52:48 -0400

    Create mysqlToSpark.scala

---
+++ SparkSql/SqlExamples/mysqlToSpark.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.SparkSession
+
+object mysqlToSpark extends App {
+  val spark = SparkSession.builder().appName("loading data from mysql").master("local[*]").getOrCreate()
+ // Class.forName("com.mysql.jdbc.Driver").newInstance
+  val mysql_car_databases= spark.read.format("jdbc").option("url","jdbc:mysql://localhost:3306/dataSources")
+                                  .option("dbtable","CarData")
+                                  .option("user","root").option("password","S@dama018")
+                                  .load().createOrReplaceGlobalTempView("cars")
+         spark.sql("select * from cars").show()
+   
+                                  
+  
+}
