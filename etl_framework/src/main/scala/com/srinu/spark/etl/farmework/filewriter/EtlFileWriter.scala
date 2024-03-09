60b4766bd6331eef18de14c7ea3da163ce77649c
sree018 <sdama018@gmail.com>
2024-03-09 10:05:02 -0500

    Delete etl_framework/src/main directory

--- etl_framework/src/main/scala/com/srinu/spark/etl/farmework/filewriter/EtlFileWriter.scala
+++
@@ -1,9 +0,0 @@
-package com.srinu.spark.etl.farmework.filewriter
-
-import com.srinu.spark.etl.farmework.logging.Logging
-import com.typesafe.config.Config
-import org.apache.spark.sql.SparkSession
-
-class EtlFileWriter(spark:SparkSession, config:Config, inputDate:String) extends Logging{
-
-}
